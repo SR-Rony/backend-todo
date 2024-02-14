791327b32abc601ff1b9c6ca25f51a6f1f77ad85
SR Rony <sr@sr.rony>
2024-02-14 15:17:42 +0600

    todo list design change

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import { twMerge } from 'tailwind-merge'
 
 const Button = ({text,className}) => {
   return (
-    <button className={twMerge("text-primary text-2xl font-semibold ring-4 ring-primary bg-transparent py-3 px-5 rounded-lg hover:bg-primary hover:text-white  ",className)}>{text}</button>
+    <button className={twMerge("text-primary text-xl font-semibold ring-2 ring-primary bg-transparent py-2 px-5 rounded-lg hover:bg-primary hover:text-white  ",className)}>{text}</button>
   )
 }
 
--- frontend/src/components/todos/Todos.jsx
+++ frontend/src/components/todos/Todos.jsx
@@ -23,29 +23,29 @@ const Todos = () => {
     //     await
     // }
 
-    myTodo()
+    // myTodo()
 
 
   return (
     <div>
-        <Heading className="text-7xl mb-10" tag="h1" text="Welcome to" span="Todo-List"/>
+        <Heading className="text-5xl mb-10" tag="h1" text="Welcome to" span="Todo-List"/>
         <form action="">
-            <input className='text-2xl py-3 px-5 ring-4 ring-primary w-1/2 rounded-lg' type="text" name='title' placeholder='write todo ...' />
+            <input className='text-xl py-2 px-5 ring-2 ring-primary w-1/2 rounded-lg' type="text" name='title' placeholder='write todo ...' />
             <Button className='mt-5 ml-5' text="Add Todo"/>
         </form>
         <List  className='mt-10 w-1/2 mx-auto border-t-4 py-5 border-b-4 border-secoundary'>
             <div className='flex justify-between items-center bg-transparent px-4 py-2 my-4 text-secoundary hover:text-white rounded-lg ring-2 ring-secoundary hover:bg-secoundary'>
                 <ListItem className='font-semibold text-2xl' text="todo list" />
                 <div>
-                    <Button className="px-5 py-2 ring-2" text='Edit'/>
-                    <Button className="px-5 py-2 ring-2 ring-red-700 ml-5 text-red-700 hover:bg-red-700" text='Delete'/>
+                    <Button className="py-1" text='Edit'/>
+                    <Button className=" py-1 ring-2 ring-red-700 ml-5 text-red-700 hover:bg-red-700" text='Delete'/>
                 </div>
             </div>
             <div className='flex justify-between items-center bg-transparent px-4 py-2 my-4 text-secoundary hover:text-white rounded-lg ring-2 ring-secoundary hover:bg-secoundary'>
                 <ListItem className='font-semibold text-2xl' text="todo list" />
                 <div>
-                    <Button className="px-5 py-2 ring-2" text='Edit'/>
-                    <Button className="px-5 py-2 ring-2 ring-red-700 ml-5 text-red-700 hover:bg-red-700" text='Delete'/>
+                    <Button className="py-1" text='Edit'/>
+                    <Button className=" py-1 ring-2 ring-red-700 ml-5 text-red-700 hover:bg-red-700" text='Delete'/>
                 </div>
             </div>
         </List>
