38696a3d88a135d8ee590a6e9762c16724ee9bb4
SR-Rony <srrony707@gmail.com>
2024-02-22 11:47:24 +0600

    todo update and edit

--- frontend/src/components/todos/Todos.jsx
+++ frontend/src/components/todos/Todos.jsx
@@ -6,33 +6,54 @@ import List from '../list/List'
 import ListItem from '../list/ListItem'
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
+import Paragraph from '../paragraph/Paragraph';
 
 const Todos = () => {
 
     const [allTodos,setAllTodos]=useState([])
     const [inpValue,setInpValue]=useState("")
+    const [addButton,setAddButton]=useState(true)
+    const [updateId,setUpdateId]=useState("")
+    // const [loding,setLoding]=useState(false)
+
 
     useEffect(()=>{
         const myTodo = async ()=>{
+            // setLoding(true)
            let todos = await axios.get("http://localhost:8000/api/todos")
           setAllTodos(todos.data.data)
         }
         myTodo()
     },[allTodos])
 
-    // handle input change
-    const handleChange =(e)=>{
-        setInpValue(e.target.value)
-        
-    }
-
     // add todo button
-    const addTodo =async()=>{
+    const handleAddTodo =async()=>{
         await axios.post("http://localhost:8000/api/todos",{
             todo:inpValue
         })
+        // .then(()=>{
+        //     setInpValue("")
+        // })
         
     }
+    // update todo button
+    const handleUpdateTodo = async()=>{
+        setAddButton(true)
+        await axios.put(`http://localhost:8000/api/todos/${updateId}`,{
+            todo:inpValue
+        })
+        // .then(()=>{
+        //     setInpValue("")
+        // })
+    }
+
+    // handle edit todo button
+    const handleEdit =(item)=>{
+        console.log(item.todo);
+        setAddButton(false)
+        // setInpValue(item.todo)
+        setUpdateId(item._id)
+    } 
 
 
     // handle delete todo button
@@ -40,21 +61,22 @@ const Todos = () => {
         await axios.delete(`http://localhost:8000/api/todos/${item._id}`)
     }
 
+    
+
 
 
   return (
     <div>
         <Heading className="text-5xl mb-10" tag="h1" text="Welcome to" span="Todo-List"/>
-            <input onChange={handleChange} className='text-xl py-2 px-5 ring-2 ring-primary w-1/2 rounded-lg' type="text" placeholder='write todo ...' />
-            <Button onClick={addTodo} className='mt-5 ml-5' text="Add Todo"/>
+            <input onChange={(e)=>setInpValue(e.target.value)} className='text-xl py-2 px-5 ring-2 ring-primary w-1/2 rounded-lg' type="text" placeholder='write todo ...' />
+            {addButton ?<Button onClick={handleAddTodo} className='mt-5 ml-5' text="Add Todo"/> : <Button onClick={handleUpdateTodo} className='mt-5 ml-5' text="Update"/>}
         <List  className='mt-10 w-1/2 mx-auto '>
+
             {allTodos.map((item)=>(
                 <div key={item._id} className='flex justify-between items-center bg-transparent px-4 py-2 my-4 text-secoundary hover:text-white rounded-lg ring-2 ring-secoundary hover:bg-secoundary'>
                     <ListItem className='font-semibold text-2xl' text={item.todo} />
-                    <div className='flex gap-4 text-2xl'>
-                    {/* <Button className="py-1" text='Edit'/> */}
-                    {/* <Button onClick={()=>handleDelete(item)} className=" py-1 ring-2 ring-red-700 ml-5 text-red-700 hover:bg-red-700" text='Delete'/> */}
-                        <FaEdit className='text-green-700 cursor-pointer'/>
+                    <div className='flex gap-4 text-2xl items-center'>
+                        <FaEdit onClick={()=>handleEdit(item)}  className='text-green-700 cursor-pointer'/>
                         <MdDelete onClick={()=>handleDelete(item)}  className='text-red-700 cursor-pointer'/>
                     </div>
                 </div>
